3fe7f7967fd8642a26c10494ece731f7c227e5a0
KEENTO <handball0321@gmail.com>
2022-10-31 09:52:26 -0700

    Added cors

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import companyRoutes from "./routes/companyRoutes";
 import seekerRoutes from "./routes/seekerRoutes";
 import authRoutes from "./routes/authRoutes";
@@ -10,8 +11,10 @@ dotenv.config();
 const app: Express = express();
 const port = 8080;
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use("/auth", authRoutes);
 app.use("/seekers", seekerRoutes);
 app.use("/companies", companyRoutes);
