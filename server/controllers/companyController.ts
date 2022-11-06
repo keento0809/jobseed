c011e925220e8c9578f5015bc27deb11faeabfc9
KEENTO <handball0321@gmail.com>
2022-11-06 14:42:32 -0800

    Setup dummy-data for testing company_controller

--- server/controllers/companyController.ts
+++ server/controllers/companyController.ts
@@ -1,10 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import { catchAsync } from "../helpers/middlewares";
+import pool from "../db/postgres";
 
 export const getAllCompanies = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const { seeker_id } = req.params;
-    if (!seeker_id) next(new Error("Invalid params"));
+    if (!seeker_id) next(new Error("Invalid seeker"));
+    const companies = await pool.query("SELECT * FROM company");
+    // if (!companies) next(new Error("Something's wrong"));
     res.status(200).json({ msg: "succeeded to get companies" });
     next();
   }
--- server/dist/controllers/companyController.js
+++ server/dist/controllers/companyController.js
@@ -8,13 +8,19 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createNewCompany = exports.getAllCompanies = void 0;
 const middlewares_1 = require("../helpers/middlewares");
+const postgres_1 = __importDefault(require("../db/postgres"));
 exports.getAllCompanies = (0, middlewares_1.catchAsync)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const { seeker_id } = req.params;
     if (!seeker_id)
-        next(new Error("Invalid params"));
+        next(new Error("Invalid seeker"));
+    const companies = yield postgres_1.default.query("SELECT * FROM company");
+    // if (!companies) next(new Error("Something's wrong"));
     res.status(200).json({ msg: "succeeded to get companies" });
     next();
 }));
