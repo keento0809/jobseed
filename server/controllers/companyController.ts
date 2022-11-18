f839981e1698bb737afe0ba326c392baef3f5bc7
KEENTO <handball0321@gmail.com>
2022-11-18 11:17:44 -0800

    Adding new functions to companyController

--- server/controllers/companyController.ts
+++ server/controllers/companyController.ts
@@ -16,6 +16,30 @@ export const getAllCompanies = catchAsync(
   }
 );
 
+export const getCompaniesWithStatus = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { status } = req.params;
+    if (
+      !status ||
+      (status != "Interested" &&
+        status != "Applied" &&
+        status !== "Interview" &&
+        status !== "Rejected")
+    )
+      next(new Error("Invalid request"));
+    const companiesWithStatusInfo = await pool.query(
+      "SELECT * FROM company WHERE company.status = $1",
+      [status]
+    );
+    if (!companiesWithStatusInfo) next(new Error("No companies found"));
+    const companiesWithStatus = companiesWithStatusInfo.rows;
+    res
+      .status(200)
+      .json({ msg: "good companies with status", companiesWithStatus });
+    next();
+  }
+);
+
 export const createNewCompany = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const { name, link, jobType, company_size, salary, location, description } =
@@ -45,6 +69,23 @@ export const createNewCompany = catchAsync(
     );
     if (!newCompany) next(new Error("Failed to create company"));
     res.status(200).json({ msg: "Company successfully created", newCompany });
+    next();
+  }
+);
+
+export const updateCompany = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { company_id } = req.params;
+    if (!company_id) next(new Error("Invalid request"));
+    const { companyObj } = req.body;
+    const { name, link, location, jobType, description, status, interest } =
+      companyObj;
+    const updatingCompany = await pool.query(
+      "UPDATE company SET name = $1,link = $2,location = $3,jobType = $4,description = $5,status = $6,interest = $7  WHERE company.company_id = $8",
+      [name, link, location, jobType, description, status, interest, company_id]
+    );
+    if (!updatingCompany) next(new Error("No company found"));
+    res.status(200).json({ msg: "Company updated" });
     next();
   }
 );
--- server/dist/controllers/seekerController.js
+++ server/dist/controllers/seekerController.js
@@ -12,14 +12,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.addAvatar = exports.updateSeekerInfo = void 0;
+exports.addAvatar = exports.updateSeekerInfo = exports.getSeekerInfo = void 0;
 const middlewares_1 = require("../helpers/middlewares");
 const postgres_1 = __importDefault(require("../db/postgres"));
+exports.getSeekerInfo = (0, middlewares_1.catchAsync)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const { seeker_id } = req.params;
+    if (!seeker_id)
+        next(new Error("Invalid request"));
+    const seekerInfo = yield postgres_1.default.query("SELECT * FROM seeker WHERE seeker.seeker_id = $1", [seeker_id]);
+    if (!seekerInfo)
+        next(new Error("No seeker found"));
+    const seeker = seekerInfo.rows[0];
+    res.status(200).json({ msg: "Good seeker", seeker });
+    next();
+}));
 exports.updateSeekerInfo = (0, middlewares_1.catchAsync)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const { seeker_id } = req.params;
+    if (!seeker_id)
+        next(new Error("Invalid request"));
     const { name, email } = req.body;
     if (!name || !email)
         next(new Error("Invalid inputs"));
-    const updatingSeeker = yield postgres_1.default.query("UPDATE seeker SET name = $1, email = $2 RETURNING *", [name, email]);
+    const updatingSeeker = yield postgres_1.default.query("UPDATE seeker SET name = $1, email = $2 WHERE seeker.seeker_id = $3 RETURNING *", [name, email, seeker_id]);
     if (!updatingSeeker)
         next(new Error("Failed to update seeker"));
     res.status(200).json({ msg: "Good seeker update", updatingSeeker });
