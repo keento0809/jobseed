963525dfb64bc3b096b9807a089a4a7dc7864683
KEENTO <handball0321@gmail.com>
2022-11-28 13:49:01 -0800

    Fixed schedule schema

--- server/db/queries/test.sql
+++ server/db/queries/test.sql
@@ -1,6 +1,8 @@
 -- CREATE TABLE seeker(seeker_id SIREAL PRIMARY KEY,);
-ALTER TABLE company ALTER COLUMN location TYPE POINT USING location::POINT;
--- ALTER TABLE company DROP COLUMN company_size;
+-- ALTER TABLE company ALTER COLUMN location TYPE POINT USING location::POINT;
+-- ALTER TABLE schedule ADD COLUMN timeIncluded BOOLEAN;
+ALTER TABLE schedule DROP COLUMN time;
+-- ALTER TABLE schedule RENAME COLUMN memo TO description;
 -- ADD COLUMN avatar TEXT DEFAULT NULL
 -- SELECT * FROM schedule;
 -- SELECT * FROM seeker;
--- server/db/seeds/schedule_seeds.sql
+++ server/db/seeds/schedule_seeds.sql
@@ -1,1 +1,1 @@
-INSERT INTO schedule(schedule_id,title,date,time,importance,memo,seeker_id,company_id) VALUES(1,'Phone interview','2022-12-01','AM 10:00',4,'Prepare the script',2,3);
+INSERT INTO schedule(schedule_id,title,date,description,seeker_id,company_id,bgColor,timeincluded) VALUES(1,'Phone interview','2022-12-01','Do my best',2,2,'blue',false);
