5b4bd3aea726e81eb2c744d460e4475f061be247
KEENTO <handball0321@gmail.com>
2022-11-30 14:43:01 -0800

    Fixed parameters for database

--- server/controllers/companyController.ts
+++ server/controllers/companyController.ts
@@ -77,11 +77,29 @@ export const updateCompany = catchAsync(
     const { company_id } = req.params;
     if (!company_id) next(new Error("Invalid request"));
     const { companyObj } = req.body;
-    const { name, link, location, jobType, description, status, interest } =
-      companyObj;
+    const {
+      name,
+      link,
+      jobtype,
+      company_size,
+      salary,
+      location,
+      description,
+      interest,
+    } = companyObj;
     const updatingCompany = await pool.query(
-      "UPDATE company SET name = $1,link = $2,location = $3,jobType = $4,description = $5,status = $6,interest = $7  WHERE company.company_id = $8",
-      [name, link, location, jobType, description, status, interest, company_id]
+      "UPDATE company SET name = $1,link = $2,jobtype = $3,company_size = $4,salary = $5,location = $6,description = $7,interest = $8 WHERE company.company_id = $9",
+      [
+        name,
+        link,
+        jobtype,
+        company_size,
+        salary,
+        location,
+        description,
+        interest,
+        company_id,
+      ]
     );
     if (!updatingCompany) next(new Error("Failed to update company"));
     res.status(200).json({ updatingCompany });
