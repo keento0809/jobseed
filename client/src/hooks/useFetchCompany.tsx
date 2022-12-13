48203443c7234a9e2e1ab0cf5a447a8df1698054
Misato Tanno <breitmeer.0804@gmail.com>
2022-12-13 14:11:38 -0800

    Modified userprofile render & S3 render function

--- client/src/hooks/useFetchCompany.tsx
+++ client/src/hooks/useFetchCompany.tsx
@@ -16,6 +16,7 @@ export const useFetchCompany = (params: AxiosRequestConfig) => {
             const result = await axios.request(params)
             const comp : any[] = result.data.companies
             comp.forEach( c => {
+                console.log(c.location)
                 c.location = {lat: parseFloat(getLat(c.location)), lng:  parseFloat(getLng(c.location))}
             })
             dispatch({type: COMPANY_ACTIONS.SUCCESS, payload: comp})
