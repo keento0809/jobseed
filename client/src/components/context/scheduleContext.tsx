c20de407bc2a30a2249aa33042b54c4951b0afbe
KEENTO <handball0321@gmail.com>
2022-12-15 11:51:42 -0800

    Fixing small issues on front-end

--- client/src/components/context/scheduleContext.tsx
+++ client/src/components/context/scheduleContext.tsx
@@ -1,101 +1,110 @@
-import React, {createContext, ReactNode, useContext, useState} from 'react';
+import React, { createContext, ReactNode, useContext, useState } from "react";
 import axios from "axios";
-import {CalenderEvent, Schedule} from "../../types/Schedule";
-import {useCookies} from "react-cookie";
+import { CalenderEvent, Schedule } from "../../types/Schedule";
+import { useCookies } from "react-cookie";
 
 type Props = {
-    children: ReactNode
+  children: ReactNode;
 };
 
 type scheduleContext = {
-    events: any[] | undefined,
-    setEvents: React.Dispatch<React.SetStateAction<any[]>>;
-    getSchedule: (id: string) => void,
-    createSchedule: (schedule: any) => void,
-    updateSchedule: (id: string, data: Schedule) => void,
-    deleteSchedule: (id: string) => void
-}
+  events: any[] | undefined;
+  setEvents: React.Dispatch<React.SetStateAction<any[]>>;
+  getSchedule: (id: string) => void;
+  createSchedule: (schedule: any) => void;
+  updateSchedule: (id: string, data: Schedule) => void;
+  deleteSchedule: (id: string) => void;
+};
 
 const scheduleContext = createContext({} as scheduleContext);
 
 export const useScheduleContext = () => {
-    return useContext(scheduleContext)
-}
+  return useContext(scheduleContext);
+};
 
-export const ScheduleProvider = ({children}: Props) => {
+export const ScheduleProvider = ({ children }: Props) => {
+  const [events, setEvents] = useState<any[]>([]);
+  const [cookies] = useCookies();
 
-    const [events, setEvents] = useState<any[]>([]);
-    const [cookies] = useCookies();
-
-    const getSchedule = async (seeker_id: string) => {
-        try {
-            let res = await axios({
-                method: "get",
-                url: `http://localhost:8080/schedules/allSchedules/${seeker_id}`,
-                withCredentials: true,
-                headers: {
-                    authorization: `Bearer ${cookies.JWT_TOKEN}`
-                }
-            })
-            await setEvents(res.data.schedules)
-        } catch (e: any) {
-            console.log(e.message)
-        }
+  const getSchedule = async (seeker_id: string) => {
+    try {
+      let res = await axios({
+        method: "get",
+        url: `http://localhost:8080/schedules/allSchedules/${cookies.SEEKER_ID}`,
+        withCredentials: true,
+        headers: {
+          authorization: `Bearer ${cookies.JWT_TOKEN}`,
+        },
+      });
+      await setEvents(res.data.schedules);
+    } catch (e: any) {
+      console.log(e.message);
     }
+  };
 
-    const createSchedule = async (data: any) => {
-        try {
-            let res = await axios({
-                method: "post",
-                url: `http://localhost:8080/schedules/new`,
-                data,
-                withCredentials: true,
-                headers: {
-                    authorization: `Bearer ${cookies.JWT_TOKEN}`
-                }
-            })
-            console.log(res.data)
-        } catch (e: any) {
-            console.log(e)
-        }
+  const createSchedule = async (data: any) => {
+    try {
+      let res = await axios({
+        method: "post",
+        url: `http://localhost:8080/schedules/new`,
+        data,
+        withCredentials: true,
+        headers: {
+          authorization: `Bearer ${cookies.JWT_TOKEN}`,
+        },
+      });
+      console.log(res.data);
+    } catch (e: any) {
+      console.log(e);
     }
+  };
 
-    const updateSchedule = async (schedule_id: string, newSchedule: Schedule) => {
-        try {
-            let res = await axios({
-                method:"put",
-                url: `http://localhost:8080/schedules/${schedule_id}`,
-                data: newSchedule,
-                withCredentials: true,
-                headers: {
-                    authorization: `Bearer ${cookies.JWT_TOKEN}`
-                }
-            })
-            console.log(res.data)
-            // setEvents(res.data)
-        } catch (e: any) {
-            console.log(e.message)
-        }
+  const updateSchedule = async (schedule_id: string, newSchedule: Schedule) => {
+    try {
+      let res = await axios({
+        method: "put",
+        url: `http://localhost:8080/schedules/${schedule_id}`,
+        data: newSchedule,
+        withCredentials: true,
+        headers: {
+          authorization: `Bearer ${cookies.JWT_TOKEN}`,
+        },
+      });
+      console.log(res.data);
+      // setEvents(res.data)
+    } catch (e: any) {
+      console.log(e.message);
     }
+  };
 
-    const deleteSchedule = async (schedule_id: string) => {
-        try {
-            await axios({
-                method: "delete",
-                url: `http://localhost:8080/schedules/${schedule_id}`,
-                withCredentials: true,
-                headers: {
-                    authorization: `Bearer ${cookies.JWT_TOKEN}`
-                }
-            })
-        } catch (err: any) {
-            console.log(err.message)
-        }
+  const deleteSchedule = async (schedule_id: string) => {
+    try {
+      await axios({
+        method: "delete",
+        url: `http://localhost:8080/schedules/${schedule_id}`,
+        withCredentials: true,
+        headers: {
+          authorization: `Bearer ${cookies.JWT_TOKEN}`,
+        },
+      });
+      await getSchedule(cookies.SEEKER_ID);
+    } catch (err: any) {
+      console.log(err.message);
     }
+  };
 
-    return(
-        <scheduleContext.Provider value={{events,setEvents,getSchedule, createSchedule, updateSchedule,deleteSchedule}}>
-            {children}
-        </scheduleContext.Provider>
-    )
-}
+  return (
+    <scheduleContext.Provider
+      value={{
+        events,
+        setEvents,
+        getSchedule,
+        createSchedule,
+        updateSchedule,
+        deleteSchedule,
+      }}
+    >
+      {children}
+    </scheduleContext.Provider>
+  );
+};
--- client/src/pages/user/Calendar.tsx
+++ client/src/pages/user/Calendar.tsx
@@ -1,84 +1,87 @@
-import React, {useEffect, useState} from 'react';
-import FullCalendar, {EventClickArg} from "@fullcalendar/react"
-import dayGridPlugin from '@fullcalendar/daygrid'
-import listPlugin from '@fullcalendar/list';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin, {DateClickArg} from "@fullcalendar/interaction";
+import React, { useEffect, useState } from "react";
+import FullCalendar, { EventClickArg } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import listPlugin from "@fullcalendar/list";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
 import EventDetail from "../../components/features/user/EventDetail";
-import {useScheduleContext} from "../../components/context/scheduleContext";
+import { useScheduleContext } from "../../components/context/scheduleContext";
 import axios from "axios";
-import {useAuthContext} from "../../components/context/AuthContext";
+import { useAuthContext } from "../../components/context/AuthContext";
+import { useCookies } from "react-cookie";
 
 const Calendar = () => {
+  const [selectedEvent, setSelectedEvent] = useState<EventClickArg>();
+  const { seekerState } = useAuthContext();
+  const { events, setEvents, getSchedule, deleteSchedule } =
+    useScheduleContext();
+  const handleClick = (arg: EventClickArg) => {
+    setSelectedEvent(arg);
+  };
+  const [cookies] = useCookies();
 
-    const [selectedEvent, setSelectedEvent] = useState<EventClickArg >();
-    const {seekerState} = useAuthContext();
-    const {events, setEvents　,getSchedule} = useScheduleContext();
-    const handleClick = (arg: EventClickArg) => {
-        setSelectedEvent(arg)
+  useEffect(() => {
+    async function getEvent() {
+      try {
+        let res = await axios({
+          method: "get",
+          url: `http://localhost:8080/schedules/allSchedules/${cookies.SEEKER_ID}`,
+          withCredentials: true,
+          headers: {
+            authorization: `Bearer ${cookies.JWT_TOKEN}`,
+          },
+        });
+        setEvents(res.data.schedules);
+      } catch (e: any) {
+        console.log(e);
+      }
     }
+    getEvent();
+  }, []);
 
-    useEffect(() => {
-       async function getEvent () {
-            try {
-                let res = await axios({
-                    method: "get",
-                    url: `http://localhost:8080/schedules/allSchedules/${seekerState.seeker.seeker_id}`,
-                    withCredentials: true,
-                    headers: {
-                        authorization: `Bearer ${seekerState.token}`
-                    }
-                })
-                setEvents(res.data.schedules)
-                console.log(events)
-            } catch (e: any) {
-                console.log(e)
-            }
-        }
-        getEvent()
-    }, [])
-
-    return (
-        <div className="relative z-5 h-[40rem]">
-            <FullCalendar
-                locale={"en"}
-                plugins={[dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin]}
-                initialView="dayGridMonth"
-                slotDuration="00:15:00"
-                weekends={true}
-                selectable={true}
-                allDayContent={true}
-                buttonIcons={false}
-                titleFormat={{
-                    year: "numeric",
-                    month: "short",
-                    day: "numeric",
-                }}
-                headerToolbar={{
-                    left: "prev,next,today",
-                    center: "title",
-                    right: 'dayGridMonth,timeGridWeek'
-                }}
-                businessHours={{
-                    daysOfWeek: [1, 2, 3, 4, 5],
-                    startTime: "0:00",
-                    endTime: "24:00",
-                }}
-                eventTimeFormat={{hour: "numeric", minute: "2-digit"}}
-                handleWindowResize={true}
-                eventClick={handleClick}
-                events={events}
-                editable={true}
-            />
-            <div className="flex justify-center">
-                {selectedEvent ? <EventDetail selectedEvent={selectedEvent}/>
-                    :
-                    <div className="w-[80%] px-6 py-3 border mt-2 shadow-xl font-thin bg-white rounded-xl">
-                        <h3 className="py-10">Click event you want to see the detail</h3>
-                    </div>}
-            </div>
-        </div>
-    );
+  return (
+    <div className="relative z-5 h-[40rem]">
+      <FullCalendar
+        locale={"en"}
+        plugins={[dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        slotDuration="00:15:00"
+        weekends={true}
+        selectable={true}
+        allDayContent={true}
+        buttonIcons={false}
+        titleFormat={{
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        }}
+        headerToolbar={{
+          left: "prev,next,today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek",
+        }}
+        businessHours={{
+          daysOfWeek: [1, 2, 3, 4, 5],
+          startTime: "0:00",
+          endTime: "24:00",
+        }}
+        eventTimeFormat={{ hour: "numeric", minute: "2-digit" }}
+        handleWindowResize={true}
+        eventClick={handleClick}
+        events={events}
+        editable={true}
+      />
+      <div className="flex justify-center">
+        {selectedEvent ? (
+          <EventDetail selectedEvent={selectedEvent} />
+        ) : (
+          <div className="w-[80%] px-6 py-3 border mt-2 shadow-xl font-thin bg-white rounded-xl">
+            <h3 className="py-10">Click event you want to see the detail</h3>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Calendar;
