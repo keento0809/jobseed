7db0b5178c9a7b340f8116faf1f7eebded6ebe8a
KEENTO <handball0321@gmail.com>
2022-12-15 21:39:31 -0800

    Fixing cors again

--- server/index.ts
+++ server/index.ts
@@ -28,6 +28,10 @@ const corsOptions = {
 //   next();
 // });
 // app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
